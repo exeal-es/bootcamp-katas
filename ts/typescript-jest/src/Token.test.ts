6247d0c438853471986ffc4fca314606bdad063b
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-10-10 18:57:05 +0200

    should be on position 97 after moving 4 when token is on position 97

--- ts/typescript-jest/src/Token.test.ts
+++ ts/typescript-jest/src/Token.test.ts
@@ -19,10 +19,19 @@ describe("Token", () => {
     expect(token.getPosition()).toBe(8);
   })
 
-  it("should be on position 100 after moving 3 when token is on postion 97", () => {
+  it("should be on position 100 after moving 3 when token is on position 97", () => {
     const token = new Token(97);
 
     token.move(3);
     expect(token.getPosition()).toBe(100);
+  })
+
+  it("should be on position 97 after moving 4 when token is on position 97", () =>{
+
+    const token = new Token(97);
+
+    token.move(4);
+    expect(token.getPosition()).toBe(97);
+ 
   })
 })
