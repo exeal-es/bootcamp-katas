163fddf4c02e03936507ca995988d13bc984f042
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-11 17:56:16 +0200

    style: lint fix

--- ts/typescript-jest/src/Token.test.ts
+++ ts/typescript-jest/src/Token.test.ts
@@ -1,43 +1,43 @@
-import { Token } from './Token';
+import { Token } from './Token'
 
-describe("Token", () => {
-  it("should should start at square one", () => {
-    const token = new Token();
-    expect(token.getPosition()).toBe(1);
+describe('Token', () => {
+  it('should should start at square one', () => {
+    const token = new Token()
+    expect(token.getPosition()).toBe(1)
   })
 
-  it("should be on position 4 after moving 3 spaces", () => {
-    const token = new Token();
-    token.move(3);
-    expect(token.getPosition()).toBe(4);
+  it('should be on position 4 after moving 3 spaces', () => {
+    const token = new Token()
+    token.move(3)
+    expect(token.getPosition()).toBe(4)
   })
 
-  it("should be on position 8 after moving 3 and then 4 spaces", () => {
-    const token = new Token();
-    token.move(3);
-    token.move(4);
-    expect(token.getPosition()).toBe(8);
+  it('should be on position 8 after moving 3 and then 4 spaces', () => {
+    const token = new Token()
+    token.move(3)
+    token.move(4)
+    expect(token.getPosition()).toBe(8)
   })
 
-  it("should be on position 100 after moving 3 when token is on position 97", () => {
-    const token = new Token(97);
+  it('should be on position 100 after moving 3 when token is on position 97', () => {
+    const token = new Token(97)
 
-    token.move(3);
-    expect(token.getPosition()).toBe(100);
+    token.move(3)
+    expect(token.getPosition()).toBe(100)
   })
 
-  it("should be on position 97 after moving 4 when token is on position 97", () =>{
+  it('should be on position 97 after moving 4 when token is on position 97', () => {
 
-    const token = new Token(97);
+    const token = new Token(97)
 
-    token.move(4);
-    expect(token.getPosition()).toBe(97);
- 
+    token.move(4)
+    expect(token.getPosition()).toBe(97)
+
   })
-  
-      it("should be a winner when the token is on position 100", () => {
-        const token = new Token(100);
 
-        expect(token.hasWon()).toBe(true);
+  it('should be a winner when the token is on position 100', () => {
+        const token = new Token(100)
+
+        expect(token.hasWon()).toBe(true)
     })
 })
--- ts/typescript-jest/src/Token.ts
+++ ts/typescript-jest/src/Token.ts
@@ -1,30 +1,28 @@
 export class Token {
 
-
-    private position: number;
+    private position: number
 
     constructor(position: number = 1) {
-        this.position = position; 
+        this.position = position
     }
 
-    getPosition(): number {
-        return this.position;
+    public getPosition(): number {
+        return this.position
+    }
+
+    public move(spaces: number): void {
+        if (!this.moveAllowed(spaces)) {
+            return
+        }
+        this.position += spaces
+    }
+
+    public hasWon(): boolean {
+        return this.position === 100
     }
 
     private moveAllowed(spaces: number): boolean {
-        return this.position + spaces <= 100;
-    }
-
-    move(spaces: number): void {
-        if (!this.moveAllowed(spaces)) {
-            return;
-        }
-        this.position += spaces;
-    }
-	
-    hasWon(): boolean {
-        return this.position === 100;
+        return this.position + spaces <= 100
     }
 
 }
-  
