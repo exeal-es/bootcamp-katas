0f9a42ee1d3eb81b5700ccaa40cb4b1332cb4df0
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-11 18:20:03 +0200

    test: fix should return a number between 1 and 6 test

--- ts/typescript-jest/src/Dice.test.ts
+++ ts/typescript-jest/src/Dice.test.ts
@@ -4,9 +4,11 @@ describe("Dice", () => {
     it("should return a number between 1 and 6", () => {
       const dice = new Dice()
 
-      const result = dice.roll()
-      expect(result).toBeGreaterThanOrEqual(1)
-      expect(result).toBeLessThanOrEqual(6)
+      for (let i = 0; i < 1000; i++) {
+        const result = dice.roll()
+        expect(result).toBeGreaterThanOrEqual(1)
+        expect(result).toBeLessThanOrEqual(6)
+      }
     })
   }
 )
