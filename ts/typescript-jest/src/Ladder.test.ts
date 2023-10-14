7b2a49202bebd154ff067e6e94bb7101a9460a9e
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-14 14:10:18 +0200

    test(ladder): should move token to top when token lands on bottom

--- ts/typescript-jest/src/Ladder.test.ts
+++ ts/typescript-jest/src/Ladder.test.ts
@@ -1,8 +1,18 @@
 import { Ladder } from './Ladder'
+import { Token } from './Token'
 
 describe('Ladder', () => {
   it('should connect two squares', () => {
     const ladder = new Ladder(2, 12)
     expect(ladder.connects(2, 12)).toBe(true)
+  })
+
+  it('should move token to top when token lands on bottom', () => {
+    const token = new Token(2)
+    const ladder = new Ladder(2, 12)
+
+    ladder.applyEffect(token)
+
+    expect(token.getPosition()).toBe(12)
   })
 })
