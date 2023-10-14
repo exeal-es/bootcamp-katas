7b2c5ed99fde9d509352da3e77941cd9e925618f
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-10-14 11:36:11 +0200

    test: should is on same position if is not a ladder

--- ts/typescript-jest/src/Ladders.test.ts
+++ ts/typescript-jest/src/Ladders.test.ts
@@ -6,3 +6,10 @@ describe('Ladders', () => {
     expect(ladder.checkLadder(2)).toBe(12)
   })
   })
+
+  describe('Ladders', () => {
+    it('should is on same position if is not a ladder', () => {
+      const ladder = new Ladders()
+      expect(ladder.checkLadder(12)).toBe(12)
+    })
+    })  
