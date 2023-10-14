fb67f50745de6929590f822e610eed9d9e587f87
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-14 13:54:12 +0200

    test(board): use domain language

--- ts/typescript-jest/src/Board.test.ts
+++ ts/typescript-jest/src/Board.test.ts
@@ -6,63 +6,43 @@ import { Snake } from './Snake'
 import { Token } from './Token'
 
 describe('Board', () => {
-  it('should return token to position 2 when token arrives at position 12' +
-    ' and there is a snake connecting 12 and 2', () => {
+  it('should move token to snake tail when token lands at snake head', () => {
+    const token = new Token(10)
+    const figures = new Set<Figure>([new Snake(12, 2)])
+    const board = new Board(token, figures)
 
-      const token = new Token(10)
-      const figures = new Set<Figure>([new Snake(12, 2)])
-      const board = new Board(token, figures)
+    board.rollDice(new FakeDice(2))
 
-      board.rollDice(new FakeDice(2))
+    expect(token.getPosition()).toBe(2)
+  })
 
-      expect(token.getPosition()).toBe(2)
-    })
+  it('should not move token to snake tail when token lands at snake tail', () => {
+    const token = new Token(1)
+    const figures = new Set<Figure>([new Snake(12, 2)])
+    const board = new Board(token, figures)
 
-  it('should return token to position 3 when token arrives at position 15' +
-    ' and there is a snake connecting 15 and 3', () => {
+    board.rollDice(new FakeDice(1))
 
-      const token = new Token(13)
-      const figures = new Set<Figure>([new Snake(15, 3)])
-      const board = new Board(token, figures)
+    expect(token.getPosition()).toBe(2)
+  })
 
-      board.rollDice(new FakeDice(2))
+  it('should move token to ladder top when token lands at ladder bottom', () => {
+    const token = new Token(1)
+    const figures = new Set<Figure>([new Ladder(2, 12)])
+    const board = new Board(token, figures)
 
-      expect(token.getPosition()).toBe(3)
-    })
+    board.rollDice(new FakeDice(1))
 
-  it('should not move token to position 12 when token arrives at position 2' +
-    ' and there is a snake connecting 12 and 2', () => {
+    expect(token.getPosition()).toBe(12)
+  })
 
-      const token = new Token(1)
-      const figures = new Set<Figure>([new Snake(12, 2)])
-      const board = new Board(token, figures)
+  it('should not move token to ladder top when token lands at ladder top', () => {
+    const token = new Token(11)
+    const figures = new Set<Figure>([new Ladder(2, 12)])
+    const board = new Board(token, figures)
 
-      board.rollDice(new FakeDice(1))
+    board.rollDice(new FakeDice(1))
 
-      expect(token.getPosition()).toBe(2)
-    })
-
-  it('should moves token to position 12 when token arrives at position 2' +
-    ' and there is a ladder connecting 2 and 12', () => {
-
-      const token = new Token(1)
-      const figures = new Set<Figure>([new Ladder(2, 12)])
-      const board = new Board(token, figures)
-
-      board.rollDice(new FakeDice(1))
-
-      expect(token.getPosition()).toBe(12)
-    })
-
-  it('should not move token to position 12 when token arrives at position 12' +
-    ' and there is a ladder connecting 2 and 12', () => {
-
-      const token = new Token(11)
-      const figures = new Set<Figure>([new Ladder(2, 12)])
-      const board = new Board(token, figures)
-
-      board.rollDice(new FakeDice(1))
-
-      expect(token.getPosition()).toBe(12)
-    })
+    expect(token.getPosition()).toBe(12)
+  })
 })
