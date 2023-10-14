2130f1e9619371fdc6092bbc83c386f3ae3262be
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-14 14:07:19 +0200

    test(snake): should move token to tail when token lands on head

--- ts/typescript-jest/src/Snake.test.ts
+++ ts/typescript-jest/src/Snake.test.ts
@@ -1,8 +1,18 @@
 import { Snake } from './Snake'
+import { Token } from './Token'
 
 describe('Snake', () => {
   it('should connect two squares', () => {
     const snake = new Snake(12, 2)
     expect(snake.connects(12, 2)).toBe(true)
+  })
+
+  it('should move token to tail when token lands on head', () => {
+    const token = new Token(12)
+    const snake = new Snake(12, 2)
+
+    snake.applyEffect(token)
+
+    expect(token.getPosition()).toBe(2)
   })
 })
