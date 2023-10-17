1f3571203b3a1b17062e7ea65fa54210728d4328
Sergio Baeza Carrasco <sergio@apparca.es>
2023-10-17 18:52:47 +0200

    feat: calculate total based on price of 1 item

---
+++ java/junit5/src/main/java/Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    public int getTotal(int price) {
+        return price;
+    }
+}
---
+++ java/junit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class CalculatorTest {
+
+    @Test
+    void calculate_price_from_item_and_price() {
+        Calculator calculator = new Calculator();
+        int expectedTotal = 10;
+        int price = 10;
+
+        int total = calculator.getTotal(price);
+
+        assertThat(expectedTotal).isEqualTo(total);
+    }
+}
