29923ac53c953ac6cd6b4967013287cccaf4f5ff
Sergio Baeza Carrasco <sergio@apparca.es>
2023-10-17 18:55:47 +0200

    feat: show on cli the total of 1 item

--- java/junit5/src/main/java/Main.java
+++ java/junit5/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo!");
+        System.out.println("Introduce el precio del objeto: ");
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        int price = scanner.nextInt();
+        int total = calculator.getTotal(price);
+
+        System.out.println("El precio total es: " + total);
+
     }
 }
