82130d854a6b7b461db45512b99292d31abcafab
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-18 18:22:36 +0200

    refactor: use parseInt to return the same number in integer

--- java/junit5/src/main/java/StringCalculator.java
+++ java/junit5/src/main/java/StringCalculator.java
@@ -3,11 +3,9 @@ public class StringCalculator {
     public static int add(String numbers) {
         switch (numbers) {
             case "3":
-                return 3;
             case "2":
-                return 2;
             case "1":
-                return 1;
+                return Integer.parseInt(numbers);
             default:
                 return 0;
         }
