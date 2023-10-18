8afc84cc6385716ee2bf5d8820462d6b61f0f68a
Sergio Baeza Carrasco <sergio@apparca.es>
2023-10-18 18:57:16 +0200

    refactor: return left number plus 2 when string contains ,

--- java/junit5/src/main/java/StringCalculator.java
+++ java/junit5/src/main/java/StringCalculator.java
@@ -4,17 +4,10 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        if (numbers.equals("3,2")) {
-            return 5;
-        }
-        if (numbers.equals("2,2")) {
-            return 4;
-        }
-        if (numbers.equals("1,2")) {
+        if (numbers.contains(",")) {
             String firstNumber = numbers.split(",")[0];
             return Integer.parseInt(firstNumber) + 2;
         }
-
         return Integer.parseInt(numbers);
     }
 }
