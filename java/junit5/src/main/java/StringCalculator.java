1aff53ca3ac538187723e146e05f22daa72cd3fe
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-18 18:24:47 +0200

    refactor: simply switch cases

--- java/junit5/src/main/java/StringCalculator.java
+++ java/junit5/src/main/java/StringCalculator.java
@@ -2,12 +2,10 @@ public class StringCalculator {
 
     public static int add(String numbers) {
         switch (numbers) {
-            case "3":
-            case "2":
-            case "1":
-                return Integer.parseInt(numbers);
-            default:
+            case "":
                 return 0;
+            default:
+                return Integer.parseInt(numbers);
         }
     }
 }
