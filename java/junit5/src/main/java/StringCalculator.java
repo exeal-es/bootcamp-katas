db589d27ea2be62302e179810a1b8f6c1eb67d42
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-18 19:20:23 +0200

    refactor: when 5,7 return sum of first plus second

--- java/junit5/src/main/java/StringCalculator.java
+++ java/junit5/src/main/java/StringCalculator.java
@@ -5,7 +5,9 @@ public class StringCalculator {
             return 0;
         }
         if (numbers.equals("5,7")) {
-            return 12;
+            String firstNumber = numbers.split(",")[0];
+            String secondNumber = numbers.split(",")[1];
+            return Integer.parseInt(firstNumber) + Integer.parseInt(secondNumber);
         }
         if (numbers.contains("2,")) {
             String secondNumber = numbers.split(",")[1];
