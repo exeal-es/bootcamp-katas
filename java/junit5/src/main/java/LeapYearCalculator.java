3884859838c50d62c4c536dafc4ffeef75955d60
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-22 17:29:54 +0200

    refactor: years divisibles by 4 and not for 100 are leap years

--- java/junit5/src/main/java/LeapYearCalculator.java
+++ java/junit5/src/main/java/LeapYearCalculator.java
@@ -1,13 +1,13 @@
 public final class LeapYearCalculator {
     public boolean isLeapYear(int year) {
         if (year == 4) {
-            return true;
+            return year % 4 == 0 && year % 100 != 0;
         }
         if (year == 8) {
-            return true;
+            return year % 4 == 0 && year % 100 != 0;
         }
         if (year == 12) {
-            return true;
+            return year % 4 == 0 && year % 100 != 0;
         }
         return year % 400 == 0;
     }
