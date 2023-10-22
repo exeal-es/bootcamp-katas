0d0729e61a0d786ca51f2bd61fa779301f8df4c0
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-22 17:06:53 +0200

    test: use parametrized test

--- java/junit5/src/test/java/LeapYearTest.java
+++ java/junit5/src/test/java/LeapYearTest.java
@@ -1,37 +1,20 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class LeapYearTest {
 
-    @Test
-    public void should_return_leap_year_for_year_400() {
-        final int year = 400;
+    @ParameterizedTest
+    @CsvSource({"400, true", "800, true", "2000, true"})
+    public void should_return_leap_year_for_years_divisible_by_400(final int year,
+        final boolean expectedResult) {
 
         final LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
         final boolean isLeapYear = leapYearCalculator.isLeapYear(year);
 
-        assertThat(isLeapYear).isTrue();
-    }
-
-    @Test
-    public void should_return_leap_year_for_year_800() {
-        final int year = 800;
-
-        final LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
-        final boolean isLeapYear = leapYearCalculator.isLeapYear(year);
-
-        assertThat(isLeapYear).isTrue();
-    }
-
-    @Test
-    public void should_return_leap_year_for_year_2000() {
-        final int year = 2000;
-
-        final LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
-        final boolean isLeapYear = leapYearCalculator.isLeapYear(year);
-
-        assertThat(isLeapYear).isTrue();
+        assertThat(isLeapYear).isEqualTo(expectedResult);
     }
 }
