1d393fdb032ab7722026bfd7bca011eea4ac9793
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-22 17:33:13 +0200

    refactor: or condition for years divisibles by 400 and for years divisbles by 4 and not for 100

--- java/junit5/src/main/java/LeapYearCalculator.java
+++ java/junit5/src/main/java/LeapYearCalculator.java
@@ -1,14 +1,5 @@
 public final class LeapYearCalculator {
     public boolean isLeapYear(int year) {
-        if (year == 4) {
-            return year % 4 == 0 && year % 100 != 0;
-        }
-        if (year == 8) {
-            return year % 4 == 0 && year % 100 != 0;
-        }
-        if (year == 12) {
-            return year % 4 == 0 && year % 100 != 0;
-        }
-        return year % 400 == 0;
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
     }
 }
