f2da44897fe217f9c02a77076b348cd21203114d
Adrián Lizaga <lizagaadrian@gmail.com>
2023-10-30 18:40:50 +0100

    style: lint fix

--- java/junit5/src/test/java/PasswordValidationShould.java
+++ java/junit5/src/test/java/PasswordValidationShould.java
@@ -3,19 +3,17 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordValidationShould {
 
-	@Test
-	public void shouldReturnTrue() {
-		ValidationResult result = PasswordValidator.isValid("exealaaaaa");
-		Assertions.assertThat(result.isValid())
-			.isTrue();
-		Assertions.assertThat(result.errors()).isEmpty();
-	}
+  @Test
+  public void shouldReturnTrue() {
+    ValidationResult result = PasswordValidator.isValid("exealaaaaa");
+    Assertions.assertThat(result.isValid()).isTrue();
+    Assertions.assertThat(result.errors()).isEmpty();
+  }
 
-	@Test
-
-	public void shouldReturnFalseWhenLengthIsBelow8() {
-		ValidationResult result = PasswordValidator.isValid("exeal");
-		Assertions.assertThat(result.isValid()).isFalse();
-		Assertions.assertThat(result.errors()).contains("Password must be at least 8 characters");
-	}
+  @Test
+  public void shouldReturnFalseWhenLengthIsBelow8() {
+    ValidationResult result = PasswordValidator.isValid("exeal");
+    Assertions.assertThat(result.isValid()).isFalse();
+    Assertions.assertThat(result.errors()).contains("Password must be at least 8 characters");
+  }
 }
