7923f80a2e93f3142a8f450c06ecb6319506084a
Pedro Pardal <ppardalj@gmail.com>
2023-10-30 19:30:11 +0100

    refactor: use linq

--- csharp/dotnet6/DotnetStarter.Logic/PasswordValidator.cs
+++ csharp/dotnet6/DotnetStarter.Logic/PasswordValidator.cs
@@ -1,21 +1,15 @@
-﻿namespace DotnetStarter.Logic;
+﻿using System.Linq;
+
+namespace DotnetStarter.Logic;
 
 public class PasswordValidator
 {
     public static ValidationResult IsValid(string password)
     {
-        int numbers = 0;
-
-        for (int i = 0; i < password.Length; i++)
-        {
-            if (char.IsDigit(password[i]))
-            {
-                numbers++;
-            }
-        }
+        var numbers = password.Count(char.IsDigit);
 
         var validationResult = new ValidationResult();
-        
+
         if (numbers <= 1)
         {
             validationResult.AddError("The password must contain at least 2 numbers");
