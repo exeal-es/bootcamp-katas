bdc542b20486b8edb5546ea14c95ea288063c0b4
Sergio Baeza Carrasco <sergio@apparca.es>
2023-10-30 18:58:54 +0100

    feat: invalid if not contains numbers

--- java/junit5/src/main/java/PasswordValidator.java
+++ java/junit5/src/main/java/PasswordValidator.java
@@ -1,6 +1,17 @@
 public class PasswordValidator {
 
   public static ValidationResult isValid(String password) {
+    int numbers = 0;
+
+    for (int i = 0; i < password.length(); i++) {
+      if (Character.isDigit(password.charAt(i))) {
+        numbers++;
+      }
+    }
+
+    if (numbers == 0) return new ValidationResult(false, "The password must contain at least 2 numbers");
+
+
     if (password.length() < 8)
       return new ValidationResult(false, "Password must be at least 8 characters");
     return new ValidationResult(true);
--- java/junit5/src/test/java/PasswordValidationShould.java
+++ java/junit5/src/test/java/PasswordValidationShould.java
@@ -1,3 +1,5 @@
+import io.vavr.control.Validation;
+import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -5,15 +7,25 @@ public class PasswordValidationShould {
 
   @Test
   public void shouldReturnTrue() {
-    ValidationResult result = PasswordValidator.isValid("exealaaaaa");
+    ValidationResult result = PasswordValidator.isValid("exe1alaaaaa");
     Assertions.assertThat(result.isValid()).isTrue();
     Assertions.assertThat(result.errors()).isEmpty();
   }
 
   @Test
   public void shouldReturnFalseWhenLengthIsBelow8() {
-    ValidationResult result = PasswordValidator.isValid("exeal");
+    ValidationResult result = PasswordValidator.isValid("exeal1");
     Assertions.assertThat(result.isValid()).isFalse();
     Assertions.assertThat(result.errors()).contains("Password must be at least 8 characters");
   }
+
+  @Test
+  public void shouldNotContainNumbers() {
+
+    ValidationResult result = PasswordValidator.isValid("notnumbersa");
+
+    Assertions.assertThat(result.isValid()).isFalse();
+    Assertions.assertThat(result.errors()).contains("The password must contain at least 2 numbers");
+  }
+
 }
