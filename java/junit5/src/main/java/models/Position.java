eb678b4713eff3234f74fef81c34cf328fd64c12
Adrián Lizaga <lizagaadrian@gmail.com>
2023-11-14 19:06:55 +0100

    refactor: make Position class instead of record

--- java/junit5/src/main/java/models/Position.java
+++ java/junit5/src/main/java/models/Position.java
@@ -1,2 +1,39 @@
 package models;
-public record Position(int x, int y){}
+
+import java.util.Objects;
+
+public final class Position {
+  private final int x;
+  private final int y;
+
+  public Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int x() {
+    return x;
+  }
+
+  public int y() {
+    return y;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (Position) obj;
+    return this.x == that.x && this.y == that.y;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y);
+  }
+
+  @Override
+  public String toString() {
+    return "Position[" + "x=" + x + ", " + "y=" + y + ']';
+  }
+}
