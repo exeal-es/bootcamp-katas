2f06ece8dc3a64684767eb48072b274d0c7fb4f3
Adrián Lizaga <lizagaadrian@gmail.com>
2023-11-21 23:25:19 +0100

    refactor: remove unused variables

--- java/junit5/src/main/java/models/MarsRover.java
+++ java/junit5/src/main/java/models/MarsRover.java
@@ -3,80 +3,78 @@ package models;
 import java.util.HashMap;
 
 public class MarsRover {
-    private final TurnRight turnRight = new TurnRight(this);
-    private final TurnLeft turnLeft = new TurnLeft(this);
-    private final MoveStraight moveStraight = new MoveStraight(this);
-    private int x;
-    private int y;
-    private char direction;
-    private final String availableDirections = "NESW";
-    private final String[] obstacles;
-    private boolean obstacleFound;
 
-    public MarsRover(int x, int y, char direction, String[] obstacles) {
-        this.x = x;
-        this.y = y;
-        this.direction = direction;
-        this.obstacles = obstacles;
+  private int x;
+  private int y;
+  private char direction;
+  private final String availableDirections = "NESW";
+  private final String[] obstacles;
+  private boolean obstacleFound;
+
+  public MarsRover(int x, int y, char direction, String[] obstacles) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.obstacles = obstacles;
+  }
+
+  public String getState() {
+    return !obstacleFound ? x + ":" + y + ":" + direction : "O:" + x + ":" + y + ":" + direction;
+  }
+
+  public void execute(Commands commands) {
+    for (Command command : commands.getCommands()) {
+      getCommandAction(command).execute();
     }
+  }
 
-    public String getState() {
-        return !obstacleFound ? x + ":" + y + ":" + direction : "O:" + x + ":" + y + ":" + direction;
-    }
+  private CommandAction getCommandAction(Command command) {
+    HashMap<Command, CommandAction> commandMap = new HashMap<>();
 
-    public void execute(Commands commands) {
-        for (Command command : commands.getCommands()) {
-            getCommandAction(command).execute();
-        }
-    }
+    commandMap.put(Command.M, new MoveStraight(this));
+    commandMap.put(Command.L, new TurnLeft(this));
+    commandMap.put(Command.R, new TurnRight(this));
 
-    private CommandAction getCommandAction(Command command) {
-        HashMap<Command, CommandAction> commandMap = new HashMap<>();
+    return commandMap.get(command);
+  }
 
-        commandMap.put(Command.M, new MoveStraight(this));
-        commandMap.put(Command.L, new TurnLeft(this));
-        commandMap.put(Command.R, new TurnRight(this));
+  public char getDirection() {
+    return direction;
+  }
 
-        return commandMap.get(command);
-    }
+  public String getAvailableDirections() {
+    return availableDirections;
+  }
 
-    public char getDirection() {
-        return direction;
-    }
+  public void setDirection(char direction) {
+    this.direction = direction;
+  }
 
-    public String getAvailableDirections() {
-        return availableDirections;
-    }
+  public String[] getObstacles() {
+    return obstacles;
+  }
 
-    public void setDirection(char direction) {
-        this.direction = direction;
-    }
+  public boolean isObstacleFound() {
+    return obstacleFound;
+  }
 
-    public String[] getObstacles() {
-        return obstacles;
-    }
+  public int getY() {
+    return y;
+  }
 
-    public boolean isObstacleFound() {
-        return obstacleFound;
-    }
+  public int getX() {
+    return x;
+  }
 
-    public int getY() {
-        return y;
-    }
+  public void setObstacleFound(boolean obstacleFound) {
+    this.obstacleFound = obstacleFound;
+  }
 
-    public int getX() {
-        return x;
-    }
+  public void setY(int y) {
+    this.y = y;
+  }
 
-    public void setObstacleFound(boolean obstacleFound) {
-        this.obstacleFound = obstacleFound;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
+  public void setX(int x) {
+    this.x = x;
+  }
 }
