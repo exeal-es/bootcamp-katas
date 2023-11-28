32dce074b6a4fef2ce20a6832d0a87d0ea84fa74
Pedro Pardal <ppardalj@gmail.com>
2023-11-28 16:21:44 +0100

    refactor: replace constructor with factory method

--- solid-kata/ocp/src/main/java/com/codurance/ocp/Employee.java
+++ solid-kata/ocp/src/main/java/com/codurance/ocp/Employee.java
@@ -6,10 +6,22 @@ public class Employee {
     private int bonus;
     private EmployeeType type;
 
-    Employee(int salary, int bonus, EmployeeType type) {
+    private Employee(int salary, int bonus, EmployeeType type) {
         this.salary = salary;
         this.bonus = bonus;
         this.type = type;
+    }
+
+    static Employee createEmployee(int salary, int bonus, EmployeeType type) {
+        return new Employee(salary, bonus, type);
+    }
+
+    static Employee createEngineer(int salary, int bonus) {
+        return createEmployee(salary, bonus, EmployeeType.ENGINEER);
+    }
+
+    static Employee createManager(int salary, int bonus) {
+        return createEmployee(salary, bonus, EmployeeType.MANAGER);
     }
 
     public int payAmount() {
