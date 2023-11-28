0e58599c0eee88d60ff60b3d89f97096d795aa59
Pedro Pardal <ppardalj@gmail.com>
2023-11-28 16:24:06 +0100

    refactor: add engineer class

--- solid-kata/ocp/src/main/java/com/codurance/ocp/Employee.java
+++ solid-kata/ocp/src/main/java/com/codurance/ocp/Employee.java
@@ -6,22 +6,18 @@ public class Employee {
     private int bonus;
     private EmployeeType type;
 
-    private Employee(int salary, int bonus, EmployeeType type) {
+    protected Employee(int salary, int bonus, EmployeeType type) {
         this.salary = salary;
         this.bonus = bonus;
         this.type = type;
     }
 
-    static Employee createEmployee(int salary, int bonus, EmployeeType type) {
-        return new Employee(salary, bonus, type);
-    }
-
     static Employee createEngineer(int salary, int bonus) {
-        return createEmployee(salary, bonus, EmployeeType.ENGINEER);
+        return new Engineer(salary, bonus);
     }
 
     static Employee createManager(int salary, int bonus) {
-        return createEmployee(salary, bonus, EmployeeType.MANAGER);
+        return new Employee(salary, bonus, EmployeeType.MANAGER);
     }
 
     public int payAmount() {
---
+++ solid-kata/ocp/src/main/java/com/codurance/ocp/Engineer.java
@@ -0,0 +1,8 @@
+package com.codurance.ocp;
+
+public class Engineer extends Employee {
+
+    public Engineer(int salary, int bonus) {
+        super(salary, bonus, EmployeeType.ENGINEER);
+    }
+}
