ac7129df56fc0c36515d741bdcbf9c3e7735d0a8
Adrián Lizaga <lizagaadrian@gmail.com>
2023-12-12 20:12:22 +0100

    feat: should greet user with good nights when hour is 21

--- java/junit5/src/main/java/Ohce.java
+++ java/junit5/src/main/java/Ohce.java
@@ -3,14 +3,21 @@ public class Ohce {
 
     private final String userName;
     private Console console;
+    private final Clock clock;
 
-    public Ohce(String userName, Console console) {
+    public Ohce(String userName, Console console, Clock clock) {
         this.userName = userName;
         this.console = console;
+        this.clock = clock;
     }
 
     public void run() {
-        console.printLine(String.format("¡Buenos días %s!", userName));
+        int hour = clock.getHour();
+        if (hour == 21) {
+            console.printLine(String.format("¡Buenas noches %s!", userName));
+        } else {
+            console.printLine(String.format("¡Buenos días %s!", userName));
+        }
         final String word = console.readLine();
         if (word != null) {
             console.printLine(reverse(word));
--- java/junit5/src/test/java/OhceTest.java
+++ java/junit5/src/test/java/OhceTest.java
@@ -12,8 +12,9 @@ public class OhceTest {
     void shouldPrintJamonReversed() {
         // given
         Console console = Mockito.mock(Console.class);
+        Clock clock = Mockito.mock(Clock.class);
         when(console.readLine()).thenReturn("jamon");
-        ohce = new Ohce("Javier", console);
+        ohce = new Ohce("Javier", console, clock);
         // when
         ohce.run();
         // then
@@ -24,8 +25,9 @@ public class OhceTest {
     void shouldPrintHolaReversed() {
         // given
         Console console = Mockito.mock(Console.class);
+        Clock clock = Mockito.mock(Clock.class);
         when(console.readLine()).thenReturn("hola");
-        ohce = new Ohce("Javier", console);
+        ohce = new Ohce("Javier", console, clock);
         // when
         ohce.run();
         // then
@@ -36,10 +38,24 @@ public class OhceTest {
   public void shouldGreetUser() {
       // given
       Console console = Mockito.mock(Console.class);
-      ohce = new Ohce("Javier", console);
+      Clock clock = Mockito.mock(Clock.class);
+      ohce = new Ohce("Javier", console, clock);
       // when
       ohce.run();
       // then
       Mockito.verify(console).printLine("¡Buenos días Javier!");
+  }
+
+  @Test
+  public void shouldGreetWithGoodNightsWhenHourIs21() {
+      // given
+      Console console = Mockito.mock(Console.class);
+      Clock clock = Mockito.mock(Clock.class);
+      Mockito.when(clock.getHour()).thenReturn(21);
+      ohce = new Ohce("Javier", console, clock);
+      // when
+      ohce.run();
+      // then
+      Mockito.verify(console).printLine("¡Buenas noches Javier!");
   }
 }
