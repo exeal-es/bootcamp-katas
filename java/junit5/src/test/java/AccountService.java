6ba2729ee172f68a6173446346c94f5e600c7c52
Pedro Pardal <ppardalj@gmail.com>
2024-06-20 11:34:47 +0200

    refactor: extract method

--- java/junit5/src/test/java/AccountService.java
+++ java/junit5/src/test/java/AccountService.java
@@ -18,17 +18,20 @@ public class AccountService {
     public void printStatement() {
         console.println("date || credit || debit || balance");
         if (!transactions.isEmpty()) {
-            Transaction t = transactions.get(0);
-            String formatDate = t.getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-            int amount = t.getAmount();
-            int credit = t.getAmount() >= 0 ? amount : 0;
-            int debit = t.getAmount() < 0 ? -amount : 0;
-            int balance = amount;
-            String formatCredit = formatNumberWithTwoDecimals(credit);
-            String formatDebit = formatNumberWithTwoDecimals(debit);
-            String formatBalance = formatNumberWithTwoDecimals(balance);
-            console.println(String.format("%s || %s || %s || %s", formatDate, formatCredit, formatDebit, formatBalance));
+            printTransaction(transactions.get(0));
         }
+    }
+
+    private void printTransaction(Transaction t) {
+        String formatDate = t.getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        int amount = t.getAmount();
+        int credit = t.getAmount() >= 0 ? amount : 0;
+        int debit = t.getAmount() < 0 ? -amount : 0;
+        int balance = amount;
+        String formatCredit = formatNumberWithTwoDecimals(credit);
+        String formatDebit = formatNumberWithTwoDecimals(debit);
+        String formatBalance = formatNumberWithTwoDecimals(balance);
+        console.println(String.format("%s || %s || %s || %s", formatDate, formatCredit, formatDebit, formatBalance));
     }
 
     private static String formatNumberWithTwoDecimals(int amount) {
