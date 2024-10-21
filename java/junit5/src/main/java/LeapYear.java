1346305ea1bd6cfb206128bc394b3d0fce00b5cb
Pedro Pardal <ppardalj@gmail.com>
2024-10-21 19:01:41 +0200

    refactor: simplify code

--- java/junit5/src/main/java/LeapYear.java
+++ java/junit5/src/main/java/LeapYear.java
@@ -1,15 +1,6 @@
 public class LeapYear {
 
   public boolean isLeapYear(int year) {
-    if (year % 400 == 0) {
-      return true;
-    }
-    if (year % 100 == 0) {
-      return false;
-    }
-    if (year % 4 == 0) {
-      return true;
-    }
-    return false;
+    return year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
   }
 }
