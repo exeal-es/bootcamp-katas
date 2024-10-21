30531c690a1d2f1fec0edff07cb8ef761f350e32
Pedro Pardal <ppardalj@gmail.com>
2024-10-21 18:35:03 +0200

    style: format code

--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -1,21 +1,18 @@
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
 
 class ThingTest {
 
-    @Test
-    void fail() {
-        Thing thing = new Thing();
-        String value = thing.callForAction();
-        assertThat(value)
-                .isEqualTo("Food");
-    }
+  @Test
+  void fail() {
+    Thing thing = new Thing();
+    String value = thing.callForAction();
+    assertThat(value).isEqualTo("Food");
+  }
 
-    @Test
-    void it_should_not_fail() {
-        assertThat(42)
-                .isEqualTo(42);
-    }
+  @Test
+  void it_should_not_fail() {
+    assertThat(42).isEqualTo(42);
+  }
 }
